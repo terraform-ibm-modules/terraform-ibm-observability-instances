7222fc3fb552cb121f6fa3bf250b55550473eef8
Mohammad Soaib <mohammad.soaib@ibm.com>
2023-03-13 21:36:26 +0530

    fix: removed un-used provider

--- examples/observability_basic/README.md
+++ examples/observability_basic/README.md
@@ -1,3 +1,3 @@
-# Provision basic observability instances (LogDNA, Sysdig, AT)
+# Provision basic observability instances (LogDNA, Sysdig, Activity Tracker)
 
 Example that deploys LogDNA, SysDig, and Activity Tracker instances with basic configuration.
--- version.tf
+++ version.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     # Use "greater than or equal to" range in modules
-    # tflint-ignore: terraform_unused_required_providers
-    ibm = {
-      source  = "ibm-cloud/ibm"
-      version = ">= 1.49.0"
-    }
     logdna = {
       source                = "logdna/logdna"
       version               = ">= 1.14.2"
