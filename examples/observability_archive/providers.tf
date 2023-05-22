8282e315cd2b07f330e9e323f0aac88b2c745a71
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-05-22 14:35:18 +0100

    feat: bump required ibm provider version to >= 1.51.0 (#283)

--- examples/observability_archive/providers.tf
+++ examples/observability_archive/providers.tf
@@ -17,19 +17,3 @@ provider "logdna" {
   servicekey = module.observability_instance_creation.logdna_resource_key != null ? module.observability_instance_creation.logdna_resource_key : ""
   url        = local.at_endpoint
 }
-
-data "ibm_iam_auth_token" "token_data" {
-}
-
-provider "restapi" {
-  uri                   = "https:"
-  write_returns_object  = false
-  create_returns_object = false
-  debug                 = false # set to true to show detailed logs, but use carefully as it might print sensitive values.
-  headers = {
-    Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
-    # Does the module have to output the value to use it here?
-    Bluemix-Instance = module.key_protect.key_protect_guid
-    Content-Type     = "application/vnd.ibm.kms.policy+json"
-  }
-}
