766403483315cf106e26bf55ead1401923eca7b5
iamar7 <md.anam.raihan17@gmail.com>
2023-05-26 14:03:30 +0000

    fix: updated moved block

--- moved.tf
+++ moved.tf
@@ -1,9 +1,13 @@
 # The following moved blocks allow consumers to upgrade from v2 of the module without instances being destroyed
 moved {
   from = logdna_archive.logdna_config
-  to   = module.log_analysis.logdna_archive.archive_config
+  to   = module.logdna.logdna_archive.archive_config
 }
 
+moved {
+  from = module.logdna.logdna_archive.archive_config
+  to   = module.log_analysis.logdna_archive.archive_config
+}
 moved {
   from = logdna_archive.activity_tracker_config
   to   = module.activity_tracker.logdna_archive.archive_config
@@ -31,11 +35,21 @@ moved {
 
 moved {
   from = ibm_resource_instance.sysdig
+  to   = module.sysdig.ibm_resource_instance.sysdig
+}
+
+moved {
+  from = module.sysdig.ibm_resource_instance.sysdig
   to   = module.cloud_monitoring.ibm_resource_instance.cloud_monitoring
 }
 
 moved {
   from = ibm_resource_key.sysdig_resource_key
+  to   = module.sysdig.ibm_resource_key.resource_key
+}
+
+moved {
+  from = module.sysdig.ibm_resource_key.resource_key
   to   = module.cloud_monitoring.ibm_resource_key.resource_key
 }
 
