557bf91050eb2a451a5a8dd0b7c5bf5a0958217c
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-09-14 21:32:02 +0100

    chore(deps): update terraform-module (#348)
    * chore(deps): update terraform-module
    
    * SKIP UPGRADE TEST
    
    ---------
    
    Co-authored-by: Conall Ó Cofaigh <ocofaigh@ie.ibm.com>

--- examples/observability_archive/main.tf
+++ examples/observability_archive/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.0.5"
+  version = "1.0.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -35,7 +35,7 @@ locals {
 
 module "cos" {
   source                     = "terraform-ibm-modules/cos/ibm"
-  version                    = "6.6.0"
+  version                    = "6.11.2"
   resource_group_id          = module.resource_group.resource_group_id
   region                     = var.region
   cos_instance_name          = "${var.prefix}-cos"
