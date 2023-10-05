519d7dd449b42722753d73209f4b14db573908e5
ocofaigh <ocofaigh@ie.ibm.com>
2023-10-05 16:36:53 +0100

    update version constraints

--- version.tf
+++ version.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.0.0, <= 1.5.5"
   required_providers {
     # Use "greater than or equal to" range in modules
     # tflint-ignore: terraform_unused_required_providers
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.56.1"
+      version = ">= 1.56.1, < 2.0.0"
     }
     logdna = {
       source                = "logdna/logdna"
-      version               = ">= 1.14.2"
+      version               = ">= 1.14.2, < 2.0.0"
       configuration_aliases = [logdna.at, logdna.ld]
     }
   }
