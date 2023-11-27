29ce3d7110a59e3a858e09e7b151d93e9f8b09f7
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-11-27 21:48:41 +0000

    chore(deps): update terraform terraform-ibm-modules/key-protect-all-inclusive/ibm to v4.4.2

--- examples/observability_archive/main.tf
+++ examples/observability_archive/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "key_protect" {
   source                    = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
-  version                   = "4.4.1"
+  version                   = "4.4.2"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   resource_tags             = var.resource_tags
