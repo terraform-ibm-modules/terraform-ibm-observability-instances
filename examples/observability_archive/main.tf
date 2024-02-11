edc7dc85edcd7a330b99d9c40674b2251baea4f8
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-02-11 08:23:29 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v7.3.0

--- examples/observability_archive/main.tf
+++ examples/observability_archive/main.tf
@@ -44,7 +44,7 @@ locals {
 
 module "cos" {
   source                     = "terraform-ibm-modules/cos/ibm"
-  version                    = "7.2.2"
+  version                    = "7.3.0"
   resource_group_id          = module.resource_group.resource_group_id
   region                     = var.region
   cos_instance_name          = "${var.prefix}-cos"
