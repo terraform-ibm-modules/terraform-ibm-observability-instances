5574f30352db58b6c173b1ecf4fae098ca6b8d11
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-04-27 20:18:19 +0000

    chore(deps): update terraform-module

--- examples/observability_archive/main.tf
+++ examples/observability_archive/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "key_protect" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "4.8.5"
+  version           = "4.9.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   resource_tags     = var.resource_tags
@@ -44,7 +44,7 @@ locals {
 
 module "cos" {
   source                     = "terraform-ibm-modules/cos/ibm"
-  version                    = "8.1.8"
+  version                    = "8.2.0"
   resource_group_id          = module.resource_group.resource_group_id
   region                     = var.region
   cos_instance_name          = "${var.prefix}-cos"
