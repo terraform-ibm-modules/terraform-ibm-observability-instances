18c8b79e6d74a6dd41f9fbc08f07bfb241c1c448
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-05-25 15:12:08 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v8.2.10

--- examples/observability_archive/main.tf
+++ examples/observability_archive/main.tf
@@ -44,7 +44,7 @@ locals {
 
 module "cos" {
   source                     = "terraform-ibm-modules/cos/ibm"
-  version                    = "8.2.9"
+  version                    = "8.2.10"
   resource_group_id          = module.resource_group.resource_group_id
   region                     = var.region
   cos_instance_name          = "${var.prefix}-cos"
