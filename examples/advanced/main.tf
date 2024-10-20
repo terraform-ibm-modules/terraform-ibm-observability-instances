3731058213e06bdfebef46a6dd1f6210c82ab382
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-10-20 07:46:21 +0100

    chore(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v4.16.3 (#594)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -21,7 +21,7 @@ locals {
 
 module "key_protect" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "4.16.2"
+  version           = "4.16.3"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   resource_tags     = var.resource_tags
