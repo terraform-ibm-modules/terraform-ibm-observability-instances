cc20e51a49cc2fa76cc3a5933916b9cfda78fced
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-12-03 10:45:34 +0000

    fix: remove vars

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -7,13 +7,11 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   type        = string
   description = "Prefix for name of all resource created by this example"
-  default     = "test-basic"
 }
 
 variable "region" {
   description = "Region where resources will be created"
   type        = string
-  default     = "us-south"
 }
 
 variable "resource_tags" {
