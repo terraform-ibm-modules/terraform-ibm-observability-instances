892bceadc574fc2f2b07cda6a492e4178f937bbf
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-16 01:12:32 +0000

    chore(deps): update terraform-module

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -44,7 +44,7 @@ module "key_protect" {
 
 module "event_notification_1" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.18.10"
+  version           = "1.18.13"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en-1"
   tags              = var.resource_tags
@@ -55,7 +55,7 @@ module "event_notification_1" {
 
 module "event_notification_2" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.18.10"
+  version           = "1.18.13"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en-2"
   tags              = var.resource_tags
@@ -75,7 +75,7 @@ locals {
 
 module "event_streams" {
   source            = "terraform-ibm-modules/event-streams/ibm"
-  version           = "3.2.0"
+  version           = "3.4.0"
   es_name           = "${var.prefix}-eventsteams-instance"
   tags              = var.resource_tags
   region            = var.region
@@ -117,7 +117,7 @@ module "event_streams" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "8.19.8"
+  version           = "8.20.2"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -132,7 +132,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "8.19.8"
+  version = "8.20.2"
   bucket_configs = [
     {
       bucket_name                   = local.logs_bucket_name
